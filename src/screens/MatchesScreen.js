4064195fac060f1e628fd4b473744cb5492c0a85
Rokas <rookas.rudzenskas@gmail.com>
2021-10-18 10:55:11 +0300

    Rokas

--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -30,7 +30,7 @@ const MatchesScreen = () => {
             return;
         }
         const fetchMatches = async () => {
-            const result = await DataStore.query(Match, m => m.isMatch('eq', true).or(m => m.User1ID('eq', me.id).User2ID('eq', me.id)),);
+            const result = await DataStore.query(Match, m => m.isMatch('eq', true).or(m1 => m1.User1ID('eq', me.id).User2ID('eq', me.id)),);
             // console.log("This is result about the users", result);
             setMatches(result);
         };
