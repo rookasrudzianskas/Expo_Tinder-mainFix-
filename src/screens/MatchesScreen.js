5bf90ea0ec125fac047b589ff8a2f479750f12c8
Rokas <rookas.rudzenskas@gmail.com>
2021-10-18 09:35:53 +0300

    Roaks

--- src/screens/MatchesScreen.js
+++ src/screens/MatchesScreen.js
@@ -10,7 +10,7 @@ const MatchesScreen = () => {
 
     useEffect(() =>  {
         const fetchMatches = async () => {
-            const result = await DataStore.query(Match, m => m.isMatch('eq', true));
+            const result = await DataStore.query(Match, m => m.isMatch('eq', true).or(m => m.User1ID('eq')));
             console.log("This is result about the users", result);
         };
         fetchMatches();
